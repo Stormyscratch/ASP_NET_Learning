5be527f78a1b4db8f4e99f727278c854e926d12b
Stormyscratch <shitintheface1305@gmail.com>
2019-03-07 15:08:56 +0700

    Commit-1

--- WebApplication1/WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/WebApplication1/App_Start/RouteConfig.cs
@@ -13,6 +13,10 @@ namespace WebApplication1
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("customHomePage", "Homepage/",new { controller = "Home", action = "Index" });
+
+            
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ WebApplication1/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        //
+        // GET: /Student/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string ShowAuthor()
+        {
+            return "Nguyen Vinh Tri";
+        }
+
+        public double Factorial(int n)
+        {
+            
+            double result = 1;
+            for (int i = 1; i < n; i++)
+            {
+                result = result * i;
+            }
+
+            return result;
+        }
+
+        public int Sum(int a, int b)
+        {
+            int result = 0;
+            return result = a + b;
+
+        }
+	}
+}
